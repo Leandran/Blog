bc800fb9cd9283ec05e1dc9ae03982bba9d87fd9
Leandran <41592532+Leandran@users.noreply.github.com>
2021-02-12 00:19:02 +0200

    added validations to model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,6 @@
 class Answer < ApplicationRecord
+    #validates that all entries are not blank 
+    validates :title, presence: true   
+    validates :author, presence: true
+    validates :body, presence: true
 end
